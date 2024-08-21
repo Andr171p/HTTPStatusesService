99482bc4666dce561c04ed84ba9d658f8ca8db82
Tyuiu.KosovAS@outlook.com <andrey.kosov.05@inbox.ru>
2024-08-22 02:33:59 +0500

    1.0

--- app/settings.py
+++ app/settings.py
@@ -7,3 +7,7 @@ class JsonResult:
     def __init__(self, message):
         self.order_json = {'order': f'{message}'}
         self.flyers_json = {'flyers': f'{message}'}
+
+
+class ServerConsts:
+    pass
